40f1b5a9eee4f8d43e46140fda150466ca709af0
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-05-30 12:54:26 +0200

    Add possibility to add new dask with some refactor

---
+++ app.js
@@ -0,0 +1,25 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplate = toDo => {
+
+const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${toDo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+`;
+
+list.innerHTML += html;
+
+}
+
+addForm.addEventListener("submit", e => {
+    
+    e.preventDefault();
+    const toDo = addForm.add.value.trim();
+    if(toDo.length) {
+    generateTemplate(toDo);
+    addForm.reset();
+    }
+});
