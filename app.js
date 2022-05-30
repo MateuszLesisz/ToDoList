28de8cd7aec0bd1c789d8b875647ee4b231a68c2
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-05-30 15:20:58 +0200

    Delete tasks after click on te rubbish

--- app.js
+++ app.js
@@ -23,3 +23,12 @@ addForm.addEventListener("submit", e => {
     addForm.reset();
     }
 });
+
+// delete todos
+
+list.addEventListener("click", e => {
+
+if(e.target.classList.contains("delete"))
+    e.target.parentElement.remove();
+
+});
